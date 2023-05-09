a6abe934d5ae1f6616231124ec9b190512543a77
Dustin West <dustinwestglow@gmail.com>
2023-05-09 00:50:22 -0500

    red hot chili peppers intro synced awesome

---
+++ Red Hot CPep HTML/storage_1.js
@@ -0,0 +1,30 @@
+var load_content = [
+    {
+        "id": "drumsticks",
+        "arr": ["One", ", 2", ", THREE", ", <u>Four!</u>"],
+        "dt": 0.6,
+        "tag": "h1",
+        "newline": true
+    },
+    {
+        "id": "callin",
+        "arr": ["Let's", " get", " it", " started!"],
+        "dt": 0.2,
+        "tag": "p",
+        "newline": false
+    },
+    {
+        "id": "yodel_1",
+        "arr": ["in", " here"],
+        "dt": 0.4,
+        "tag": "span",
+        "newline": false
+    },
+    {
+        "id": "yodel_2",
+        "arr": Array(10).fill("e"),
+        "dt": 0.1,
+        "tag": "span",
+        "newline": true
+    }
+    ];
